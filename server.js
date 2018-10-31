fb8c2570ab1d4b93b689ba9b59908ad782a0dfb5
OlesjaSenkiv <olesja.senkiv@ttu.ee>
2018-10-31 09:26:42 +0200

    #50

--- server.js
+++ server.js
@@ -163,8 +163,8 @@ function render (req, res) {
     <!--&lt;!&ndash;<script type="text/javascript" src="/static/js/scrollreveal.min.js"></script>&ndash;&gt;-->
     <script src="https://cdn.jsdelivr.net/lodash/4.13.1/lodash.js"></script>
     <!--<script type="text/javascript" src="/static/ol3/ol.js"></script>-->
-    <script type="text/javascript" src="/static/leaflet-1.3.4/leaflet.js"></script>
-    <script type="text/javascript" src="/static/leaflet-fullscreen/leaflet.fullscreen.js"></script>
+    <script src="/static/leaflet-1.3.4/leaflet.js"></script>
+    <script src="/static/leaflet-fullscreen/Leaflet.fullscreen.js"></script>
     <script type="text/javascript" src="/static/js/fossils.js"></script>
     <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.4.2/jquery.fancybox.min.js"></script>
