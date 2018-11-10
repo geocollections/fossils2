9151cb2e4601532ca9662713b75f1a41361d5398
Olle Hints <olle.hints@ttu.ee>
2018-11-10 16:25:55 +0200

    Update ItemPage.vue

--- src/views/ItemPage.vue
+++ src/views/ItemPage.vue
@@ -14,7 +14,7 @@
            <b-row class="ml-0">
                <div>
                <a :href="'/'+taxon.fossil_group__id" v-if="taxon.fossil_group__id != null">
-                   <img border="0" :src="'/static/fossilgroups/'+taxon.fossil_group__id+'.png'" :alt="taxon.fossil_group__taxon" :title="taxon.fossil_group__taxon" style="height: 80px; margin-top: 0px; padding-right: 0px;" /></a>
+                   <img border="0" :src="'/static/fossilgroups/'+taxon.fossil_group__id+'.png'" :alt="taxon.fossil_group__taxon" :title="taxon.fossil_group__taxon" style="height: 120px; margin: 0 20px 0 0; padding-right: 0px;" /></a>
                <a :href="'/'+taxon.id" v-else-if="taxon.is_fossil_group === 1">
                    <img border="0" :src="'/static/fossilgroups/'+ taxon.id+'.png'" :alt="taxon.taxon" :title="taxon.taxon" style="height: 95px; margin-top: 0; padding-right: 0px;" /></a>
                </div>
