673a27995f942451aaae326d14c289914f1e3b19
OlesjaSenkiv <olesja.senkiv@ttu.ee>
2018-11-13 15:19:02 +0200

    dropdown menu right align

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,26 +22,25 @@
                               :results-display="displayResults"
                               :placeholder="$t('search.fossils_search')"
                               @selected="onSelect"></autocomplete></form>
-              <b-nav-item-dropdown>
+              <b-nav-item-dropdown  right>
                 <template slot="button-content" >{{$t(mode)}}</template>
                 <b-dropdown-item @click="changeMode('in_estonia')" :class="$store.state.mode === 'in_estonia'? 'font-weight-bold' : ''">{{$t('header.in_estonia_mode')}}</b-dropdown-item>
                 <b-dropdown-item @click="changeMode('in_baltoscandia')" :class="$store.state.mode === 'in_baltoscandia'? 'font-weight-bold' : ''">{{$t('header.in_baltoscandia_mode')}}</b-dropdown-item>
                 <b-dropdown-item @click="changeMode('in_global')" :class="$store.state.mode === 'in_global'? 'font-weight-bold' : ''">{{$t('header.global_mode')}}</b-dropdown-item>
               </b-nav-item-dropdown>
-              <b-nav-item-dropdown>
+              <b-nav-item-dropdown  right>
                 <template slot="button-content" >{{langCode}}</template>
-
                 <b-dropdown-item  @click="changeLang('ee')" :class="$store.state.lang === 'et'? 'font-weight-bold' : ''" class="p-2">EST &nbsp;<span class="flag-icon flag-icon-ee flag-icon-squared circle-flag"></span></b-dropdown-item>
                 <b-dropdown-item  @click="changeLang('en')" :class="$store.state.lang === 'en'? 'font-weight-bold' : ''" class="p-2">ENG &nbsp;<span class="flag-icon flag-icon-gb flag-icon-squared circle-flag"></span></b-dropdown-item>
                 <b-dropdown-item  @click="changeLang('fi')" :class="$store.state.lang === 'fi'? 'font-weight-bold' : ''" class="p-2">FIN &nbsp;<span class="flag-icon flag-icon-fi flag-icon-squared circle-flag"></span></b-dropdown-item>
                 <b-dropdown-item  @click="changeLang('se')" :class="$store.state.lang === 'se'? 'font-weight-bold' : ''" class="p-2">SWE &nbsp;<span class="flag-icon flag-icon-se flag-icon-squared circle-flag"></span></b-dropdown-item>
               </b-nav-item-dropdown>
-              <b-nav-item-dropdown>
+              <b-nav-item-dropdown right>
                 <template slot="button-content">{{ $t('menu.more') }}</template>
-                <b-dropdown-item  href="/page/28" class="p-2">{{ $t('menu.fossils') }}</b-dropdown-item>
-                <b-dropdown-item  href="/page/29" class="p-2">{{ $t('menu.collecting') }}</b-dropdown-item>
-                <b-dropdown-item  href="/page/30" class="p-2">{{ $t('menu.identifying') }}</b-dropdown-item>
-                <b-dropdown-item  href="http://geocollections.info" class="p-2">{{ $t('menu.geocollections') }}</b-dropdown-item>
+                <b-dropdown-item  href="/page/28">{{ $t('menu.fossils') }}</b-dropdown-item>
+                <b-dropdown-item  href="/page/29">{{ $t('menu.collecting') }}</b-dropdown-item>
+                <b-dropdown-item  href="/page/30">{{ $t('menu.identifying') }}</b-dropdown-item>
+                <b-dropdown-item  href="http://geocollections.info">{{ $t('menu.geocollections') }}</b-dropdown-item>
               </b-nav-item-dropdown>
             </b-navbar-nav>
           </b-collapse>
@@ -101,7 +100,9 @@
           this.$router.push({ path: this.$router.currentRoute.path, query:  {mode:this.$store.state.mode, lang: lang}  });
       },
       simpleTaxonSearchApiCall(value) {
-          return 'https://api.geocollections.info/solr/fossils_search/?q=name:*'+ value +'*&f1=id,name';
+          return 'https://api.geocollections.info/solr/fossils_search/?q=name:*'+ value +'*';
+          //return 'https://api.geocollections.info/taxon/?paginate_by=10&fields=id,taxon,common_name__name,rank__rank_short&multi_search=value:' + value + ';fields:taxon,common_name__name;lookuptype:icontains'
+        // return 'https://api.geocollections.info/taxon/?paginate_by=30&format=json&fields=id,taxon,rank__rank_en&multi_search=value:' + value + ';fields:taxon;lookuptype:icontains'
       },
       displayResults: function (result) {
           return result.name[0]
