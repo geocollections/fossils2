6a7b9be645275295f7ca3f6700c7230d525430e9
OlesjaSenkiv <olesja.senkiv@ttu.ee>
2018-11-22 14:37:50 +0200

    style adjustments

--- src/components/VueFoldable.vue
+++ src/components/VueFoldable.vue
@@ -192,10 +192,11 @@
       &.collapsed
         opacity 1
         left 0
-        background -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, rgba(55, 55, 55, 0)), color-stop(100%, #FFFFFF));
-        background -moz-linear-gradient(rgba(55, 55, 55, 0), #FFFFFF)
-        background -webkit-linear-gradient(to bottom, rgba(55, 55, 55, 0), #FFFFFF)
-        background linear-gradient(to bottom, rgba(55, 55, 55, 0), #FFFFFF)
+        background -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, rgba(255, 255, 255, 0)), color-stop(100%, #FFFFFF));
+        background -moz-linear-gradient(rgba(255, 255, 255, 0), #FFFFFF)
+        background -webkit-linear-gradient(to bottom, rgba(255, 255, 255, 0), #FFFFFF)
+        background linear-gradient(to bottom, rgba(255, 255, 255, 0), #FFFFFF)
+
       &:not(.collapsed)
         opacity 0
 
